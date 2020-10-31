d37017bfa6bdd544a65bfb1131e1b51e43bb1b1d
quangnst <quangnst@gmail.com>
2020-10-31 17:36:45 +0700

    add source nodejs express mongo vue todo

---
+++ backend/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+   db: 'mongodb://interface_db:452vanhanh@cluster0-shard-00-00.llsqc.mongodb.net:27017,cluster0-shard-00-01.llsqc.mongodb.net:27017,cluster0-shard-00-02.llsqc.mongodb.net:27017/shopping?ssl=true&replicaSet=atlas-21f2ey-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
